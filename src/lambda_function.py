589371e699a3da4fa718e392bbc3f16fc2640ea7
blavelle-portfolio <106982674+blavelle-portfolio@users.noreply.github.com>
2022-10-29 13:02:56 -0400

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,8 +4,8 @@ import boto3
 def start_ec2():
     ec2 = boto3.resource('ec2')
 
-    tag_key = "gpu"
-    tag_val = "yes"
+    tag_key = 'gpu'
+    tag_val = 'yes'
     instances = ec2.describe_instances(Filters=[{'Name': 'tag:'+tag_key, 'Values': [tag_val]}])
 
     try:
@@ -19,13 +19,23 @@ def add_variables_to_ssm(to_be_added):
     
     try:
         resposne = ssm.put_parameter(
-            Name=to_be_added
-            Description='added from lambda',
+            Name=to_be_added,
+            Description=to_be_added,
             Value=to_be_added,
             Type='String',
+            KeyId='string',
             Overwrite=True,
+            AllowedPattern='string',
+            Tags=[
+                {
+                    'Key': 'gpu',
+                    'Value': 'yes'
+                },
+            ],
             Tier='Standard',
-            DataType='string')
+            Policies='string',
+            DataType='string'
+        )
         return response
     except CLientError as e:
         return e
@@ -35,16 +45,10 @@ def lambda_handler(event, context):
     # from form-data
     algo_of_hash = json.loads["Metadata"]["algo"]
     hash_to_crack = json.loads["Metadata"]["hash"]
-    wordlist = json.loads["Metadata"]{"wordlist"}
-
-    try: 
-        start_ec2()
-        add_variables_to_ssm(algo_of_hash)
-        add_variables_to_ssm(hash_to_crack)
-        add_variables_to_ssm(wordlist) 
-        
-        return {"statusCode": 200, "body": json.dumps("Hash uploaded successfully! Please check your email for the output")}
+    wordlist = json.loads["Metadata"]["wordlist"]
     
-    except CLientError as e:
-        return e
-
+    start_ec2()
+    add_variables_to_ssm(algo_of_hash)
+    add_variables_to_ssm(hash_to_crack)
+    add_variables_to_ssm(wordlist) 
+        
