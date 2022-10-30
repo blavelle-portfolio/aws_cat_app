c5aa2b037dbe15080454e0aa5dc9e2c3d596324b
blavelle-portfolio <106982674+blavelle-portfolio@users.noreply.github.com>
2022-10-30 10:57:57 -0400

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,18 +1,14 @@
-import json
 import boto3
-from botocore import exceptions
+
 
 def start_ec2():
-
-    desc = boto3.client('ec2')
     ec2 = boto3.resource('ec2')
 
-    tag_key = 'gpu'
-    tag_val = 'yes'
-        
-    instances = desc.describe_instances(Filters=[{'Name': 'tag:'+tag_key, 'Values': [tag_val]}])
-   
-    ec2.start_instances(InstanceIds=[instances], DryRun=False)
+    ec2_instance = {"Name": "instance-type", "Values": ["g4dn.2xlarge"]}
+    ec2_tag = {"Name": "tag:Name", "Values": ["hashcat_box"]}
+
+    for instance in ec2.instances.filter(Filters=[ec2_tag]):
+        instance.start()
 
 def add_variables_to_ssm(to_be_added):
     ssm = boto3.client('ssm')
@@ -21,18 +17,8 @@ def add_variables_to_ssm(to_be_added):
             Description=to_be_added,
             Value=to_be_added,
             Type='String',
-            KeyId='string',
             Overwrite=True,
-            AllowedPattern='string',
-            Tags=[
-                {
-                    'Key': 'gpu',
-                    'Value': 'yes'
-                },
-            ],
-            Tier='Standard',
-            Policies='string',
-            DataType='string'
+            Tier='Standard'
         )
     
 
@@ -40,7 +26,7 @@ def lambda_handler(event, context):
     # from form-data
     algo_of_hash = event['algo']
     hash_to_crack = event['hash']
-    wordlist = event = ['wordlist']
+    wordlist = event['wordlist']
     
     start_ec2()
 
